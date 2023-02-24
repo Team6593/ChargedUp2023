83d0d0f109ababa7a02fc0fe08f21362a845dcaf
Mansour Quddus <itsnothisuser@gmail.com>
2023-02-24 18:31:20 -0500

    NavX subsystem javadoc

--- src/main/java/frc/robot/subsystems/NavX.java
+++ src/main/java/frc/robot/subsystems/NavX.java
@@ -13,7 +13,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class NavX extends SubsystemBase {
   public AHRS navX;
 
-  /** Creates a new NavX. */
+  /** Creates a new NavX. 
+   *  On the creation of the constructor, an attempt to create a AHRS object will be made.
+   *  If the NavX-kMXP can't be found, an exception will be thrown
+  */
   public NavX() {
     try {
       navX = new AHRS(SPI.Port.kMXP);
@@ -40,14 +43,34 @@ public class NavX extends SubsystemBase {
     navX.zeroYaw();
   } 
 
+  /**
+   * depending on the orientation of the NavX, the pitch may actually be the roll instead
+   * this occurs beacuse the RoboRIO is installed onto the robot sideways, so the roll and
+   * the pitch are swapped, regardless the implemenetation of getPitch() and getRoll() will
+   * not change.
+   * @return pitch in degrees
+   */
   public float getPitch() {
     return navX.getPitch();
   }
 
+  /**
+   * getAngle() and getYaw() return the same number (the yaw in degrees), 
+   * however, getAngle() will return a more precise measurement because
+   * it returns a double precision number.
+   * @return The current total accumulated yaw angle (Z axis) of the robot in degrees. 
+   * This heading is based on integration of the returned rate from the Z-axis (yaw) gyro.
+   */
   public double getAngle() {
     return navX.getAngle();
   }
 
+  /**
+   * getAngle() and getYaw() return the same number (the yaw in degrees), 
+   * however, getAngle() will return a more precise measurement because
+   * it returns a double precision number.
+   * @return the yaw in degrees
+   */
   public float getYaw() {
     return navX.getYaw();
   }
@@ -68,6 +91,14 @@ public class NavX extends SubsystemBase {
     return navX.getVelocityZ();
   }
 
+  /**
+   * depending on the orientation of the NavX, the pitch may actually be the roll instead
+   * this occurs beacuse the RoboRIO is installed onto the robot sideways, so the roll and
+   * the pitch are swapped, regardless the implemenetation of getPitch() and getRoll() will
+   * not change.
+   * 
+   * @return roll in degrees
+   */
   public float getRoll() {
     return navX.getRoll();
   }
