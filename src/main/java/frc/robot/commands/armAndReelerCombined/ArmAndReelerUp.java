4e6ab3cede34fcf9a7f751a5decab412561866a0
KelvinBM <kelvinmejia182@gmail.com>
2023-03-06 16:44:42 -0500

    working on parallel and sequential commands for arm and reeler subsystems

--- src/main/java/frc/robot/commands/armAndReelerCombined/ArmAndReelerUp.java
+++ src/main/java/frc/robot/commands/armAndReelerCombined/ArmAndReelerUp.java
@@ -2,23 +2,16 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.armAndReelerSequential;
+package frc.robot.commands.armAndReelerCombined;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.Arm;
-import frc.robot.subsystems.Reeler;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class ArmUpExtendGrab extends SequentialCommandGroup {
-  private Reeler reeler;
-  private Arm arm;
-  private double reelerSpeed;
-  private double armSpeed;
-  
-  /** Creates a new ArmUpExtendGrab. */
-  public ArmUpExtendGrab() {
+public class ArmAndReelerUp extends ParallelCommandGroup {
+  /** Creates a new ArmAndReelerUp. */
+  public ArmAndReelerUp() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands();
---
+++ src/main/java/frc/robot/commands/armAndReelerCombined/ArmUpExtendGrab.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.armAndReelerCombined;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.arm.ArmClose;
+import frc.robot.commands.arm.ArmOpen;
+import frc.robot.commands.reeler.ReelArmDown;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Reeler;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class ArmUpExtendGrab extends SequentialCommandGroup {
+  private Reeler reeler;
+  private Arm arm;
+  private double reelerSpeed;
+  private double armSpeed;
+
+  /** Creates a new ArmUpExtendGrab. */
+  public ArmUpExtendGrab(Reeler reeler, Arm arm, double reelerSpeed, double armSpeed) {
+    this.reeler = reeler;   
+    this.arm = arm;
+    this.reelerSpeed = reelerSpeed;
+    this.armSpeed = armSpeed;
+    
+    addRequirements(arm, reeler);
+    
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(new ArmAndReelerUp(arm, armSpeed, reeler, reelerSpeed),
+                new ArmClose(arm)
+    );
+  }
+}
